68ef0428c5c77a22d526c97b26c33d20bb238422
bossonfields <lwh14710@gmail.com>
2018-04-15 19:25:46 +0800

    Update Emojify.py

--- Emojify/Emojify.py
+++ Emojify/Emojify.py
@@ -205,5 +205,159 @@ Actual
 All          9    9   19   13    6   56
 
 
+#Implmenting Emojifier_v2
+import numpy as np
+np.random.seed(0)
+from keras.models import Model
+from keras.layers import Dense, Input, Dropout, LSTM, Activation
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.initializers import glorot_uniform
+np.random.seed(1)
 
+
+# sentences_to_indices
+
+def sentences_to_indices(X, word_to_index, max_len):
+    
+    m = X.shape[0]                                   # number of training examples
+    
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape (≈ 1 line)
+    X_indices = np.zeros((m, max_len))
+    
+    for i in range(m):                               # loop over training examples
+        
+        # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+        sentence_words = X[i].lower().split()
+        
+        # Initialize j to 0
+        j = 0
+        
+        # Loop over the words of sentence_words
+        for w in sentence_words:
+            # Set the (i,j)th entry of X_indices to the index of the correct word.
+            X_indices[i, j] = word_to_index[w]
+            # Increment j to j + 1
+            j = j+1
+    
+    return X_indices
+
+#test
+X1 = np.array(["funny lol", "lets play baseball", "food is ready for you"])
+X1_indices = sentences_to_indices(X1,word_to_index, max_len = 5)
+print("X1 =", X1)
+print("X1_indices =", X1_indices)
+
+#output
+X1 = ['funny lol' 'lets play baseball' 'food is ready for you']
+X1_indices = [[ 155345.  225122.       0.       0.       0.]
+ [ 220930.  286375.   69714.       0.       0.]
+ [ 151204.  192973.  302254.  151349.  394475.]]
+
+
+# pretrained_embedding_layer
+
+def pretrained_embedding_layer(word_to_vec_map, word_to_index):
+
+    vocab_len = len(word_to_index) + 1                  # adding 1 to fit Keras embedding (requirement)
+    emb_dim = word_to_vec_map["cucumber"].shape[0]      # define dimensionality of your GloVe word vectors (= 50)
+    
+    # Initialize the embedding matrix as a numpy array of zeros of shape (vocab_len, dimensions of word vectors = emb_dim)
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+    
+    # Set each row "index" of the embedding matrix to be the word vector representation of the "index"th word of the vocabulary
+    for word, index in word_to_index.items():
+        emb_matrix[index, :] = word_to_vec_map[word]
+
+    # Define Keras embedding layer with the correct output/input sizes, make it trainable. Use Embedding(...). Make sure to set trainable=False. 
+    embedding_layer = Embedding(vocab_len , emb_dim, trainable=False)
+
+    # Build the embedding layer, it is required before setting the weights of the embedding layer. Do not modify the "None".
+    embedding_layer.build((None,))
+    
+    # Set the weights of the embedding layer to the embedding matrix. Your layer is now pretrained.
+    embedding_layer.set_weights([emb_matrix])
+    
+    return embedding_layer
+
+#test
+embedding_layer = pretrained_embedding_layer(word_to_vec_map, word_to_index)
+print("weights[0][1][3] =", embedding_layer.get_weights()[0][1][3])
+
+#output
+weights[0][1][3] = -0.3403
+
+
+# Emojify_V2
+
+def Emojify_V2(input_shape, word_to_vec_map, word_to_index):
+
+    # Define sentence_indices as the input of the graph, it should be of shape input_shape and dtype 'int32' (as it contains indices).
+    sentence_indices = Input(shape = input_shape, dtype = np.int32 )
+    
+    # Create the embedding layer pretrained with GloVe Vectors (≈1 line)
+    embedding_layer = pretrained_embedding_layer(word_to_vec_map, word_to_index)
+    
+    # Propagate sentence_indices through your embedding layer, you get back the embeddings
+    embeddings = embedding_layer(sentence_indices)   
+    
+    # Propagate the embeddings through an LSTM layer with 128-dimensional hidden state
+    # Be careful, the returned output should be a batch of sequences.
+    X = LSTM(128, return_sequences = True)(embeddings)
+    # Add dropout with a probability of 0.5
+    X = Dropout(0.5)(X)
+    # Propagate X trough another LSTM layer with 128-dimensional hidden state
+    # Be careful, the returned output should be a single hidden state, not a batch of sequences.
+    X = LSTM(128, return_sequences = False)(X)
+    # Add dropout with a probability of 0.5
+    X = Dropout(0.5)(X)
+    # Propagate X through a Dense layer with softmax activation to get back a batch of 5-dimensional vectors.
+    X = Dense(5)(X)
+    # Add a softmax activation
+    X = Activation('softmax')(X)
+    
+    # Create Model instance which converts sentence_indices into X.
+    model = Model(inputs = sentence_indices, outputs = X)
+    
+    return model
+
+#test
+model = Emojify_V2((maxLen,), word_to_vec_map, word_to_index)
+model.summary()
+
+#train model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+X_train_indices = sentences_to_indices(X_train, word_to_index, maxLen)
+Y_train_oh = convert_to_one_hot(Y_train, C = 5)
+
+model.fit(X_train_indices, Y_train_oh, epochs = 50, batch_size = 32, shuffle=True)
+
+X_test_indices = sentences_to_indices(X_test, word_to_index, max_len = maxLen)
+Y_test_oh = convert_to_one_hot(Y_test, C = 5)
+loss, acc = model.evaluate(X_test_indices, Y_test_oh)
+print()
+print("Test accuracy = ", acc)
+
+#output
+32/56 [================>.............] - ETA: 0s
+Test accuracy =  0.892857142857
+
+
+# This code allows you to see the mislabelled examples
+C = 5
+y_test_oh = np.eye(C)[Y_test.reshape(-1)]
+X_test_indices = sentences_to_indices(X_test, word_to_index, maxLen)
+pred = model.predict(X_test_indices)
+for i in range(len(X_test)):
+    x = X_test_indices
+    num = np.argmax(pred[i])
+    if(num != Y_test[i]):
+        print('Expected emoji:'+ label_to_emoji(Y_test[i]) + ' prediction: '+ X_test[i] + label_to_emoji(num).strip())
+
+
+# Change the sentence below to see your prediction. Make sure all the words are in the Glove embeddings.  
+x_test = np.array(['not feeling happy'])
+X_test_indices = sentences_to_indices(x_test, word_to_index, maxLen)
+print(x_test[0] +' '+  label_to_emoji(np.argmax(model.predict(X_test_indices))))
 
