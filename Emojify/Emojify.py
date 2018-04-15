16f5632af03f32c006ef74d29dee582c6e0dc2ee
bossonfields <lwh14710@gmail.com>
2018-04-15 19:19:47 +0800

    Create Emojify.py

---
+++ Emojify/Emojify.py
@@ -0,0 +1,209 @@
+import numpy as np
+from emo_utils import *
+import emoji
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+X_train, Y_train = read_csv('data/train_emoji.csv')
+X_test, Y_test = read_csv('data/tesss.csv')
+
+maxLen = len(max(X_train, key=len).split())
+
+index = 5
+print(X_train[index], label_to_emoji(Y_train[index]))
+
+#output
+I love you mum(emoji)
+
+Y_oh_train = convert_to_one_hot(Y_train, C = 5)
+Y_oh_test = convert_to_one_hot(Y_test, C = 5)
+
+index = 70
+print(Y_train[index], "is converted into one hot", Y_oh_train[index])
+
+#output
+1 is converted into one hot [ 0.  1.  0.  0.  0.]
+
+word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+word = "cucumber"
+index = 289846
+print("the index of", word, "in the vocabulary is", word_to_index[word])
+print("the", str(index) + "th word in the vocabulary is", index_to_word[index])
+
+#output
+the index of cucumber in the vocabulary is 113317
+the 289846th word in the vocabulary is potatos
+
+
+#Sentence to avg
+def sentence_to_avg(sentence, word_to_vec_map):
+
+    # Step 1: Split sentence into list of lower case words (≈ 1 line)
+    words = (sentence.lower()).split()
+
+    # Initialize the average word vector, should have the same shape as your word vectors.
+    avg = np.zeros((50))
+    
+    # Step 2: average the word vectors. You can loop over the words in the list "words".
+    for w in words:
+        avg += word_to_vec_map[w]
+    avg = avg/len(words)
+    
+    return avg
+
+#test
+avg = sentence_to_avg("Morrocan couscous is my favorite dish", word_to_vec_map)
+print("avg = ", avg)
+
+#output
+avg =  [-0.008005    0.56370833 -0.50427333  0.258865    0.55131103  0.03104983
+ -0.21013718  0.16893933 -0.09590267  0.141784   -0.15708967  0.18525867
+  0.6495785   0.38371117  0.21102167  0.11301667  0.02613967  0.26037767
+  0.05820667 -0.01578167 -0.12078833 -0.02471267  0.4128455   0.5152061
+  0.38756167 -0.898661   -0.535145    0.33501167  0.68806933 -0.2156265
+  1.797155    0.10476933 -0.36775333  0.750785    0.10282583  0.348925
+ -0.27262833  0.66768    -0.10706167 -0.283635    0.59580117  0.28747333
+ -0.3366635   0.23393817  0.34349183  0.178405    0.1166155  -0.076433
+  0.1445417   0.09808667]
+
+
+# model
+
+def model(X, Y, word_to_vec_map, learning_rate = 0.01, num_iterations = 400):
+
+    np.random.seed(1)
+
+    # Define number of training examples
+    m = Y.shape[0]                          # number of training examples
+    n_y = 5                                 # number of classes  
+    n_h = 50                                # dimensions of the GloVe vectors 
+    
+    # Initialize parameters using Xavier initialization
+    W = np.random.randn(n_y, n_h) / np.sqrt(n_h)
+    b = np.zeros((n_y,))
+    
+    # Convert Y to Y_onehot with n_y classes
+    Y_oh = convert_to_one_hot(Y, C = n_y) 
+    
+    # Optimization loop
+    for t in range(num_iterations):                       # Loop over the number of iterations
+        for i in range(m):                                # Loop over the training examples
+            
+            # Average the word vectors of the words from the i'th training example
+            avg = sentence_to_avg(X[i], word_to_vec_map)
+
+            # Forward propagate the avg through the softmax layer
+            z = np.dot(W, avg) + b
+            a = softmax(z)
+
+            # Compute cost using the i'th training label's one hot representation and "A" (the output of the softmax)
+            cost = - np.sum(Y_oh[i] * np.log(a))
+            
+            # Compute gradients 
+            dz = a - Y_oh[i]
+            dW = np.dot(dz.reshape(n_y,1), avg.reshape(1, n_h))
+            db = dz
+
+            # Update parameters with Stochastic Gradient Descent
+            W = W - learning_rate * dW
+            b = b - learning_rate * db
+        
+        if t % 100 == 0:
+            print("Epoch: " + str(t) + " --- cost = " + str(cost))
+            pred = predict(X, Y, W, b, word_to_vec_map)
+
+    return pred, W, b
+
+#test
+print(X_train.shape)
+print(Y_train.shape)
+print(np.eye(5)[Y_train.reshape(-1)].shape)
+print(X_train[0])
+print(type(X_train))
+Y = np.asarray([5,0,0,5, 4, 4, 4, 6, 6, 4, 1, 1, 5, 6, 6, 3, 6, 3, 4, 4])
+print(Y.shape)
+
+X = np.asarray(['I am going to the bar tonight', 'I love you', 'miss you my dear',
+ 'Lets go party and drinks','Congrats on the new job','Congratulations',
+ 'I am so happy for you', 'Why are you feeling bad', 'What is wrong with you',
+ 'You totally deserve this prize', 'Let us go play football',
+ 'Are you down for football this afternoon', 'Work hard play harder',
+ 'It is suprising how people can be dumb sometimes',
+ 'I am very disappointed','It is the best day in my life',
+ 'I think I will end up alone','My life is so boring','Good job',
+ 'Great so awesome'])
+
+print(X.shape)
+print(np.eye(5)[Y_train.reshape(-1)].shape)
+print(type(X_train))
+
+#output
+(132,)
+(132,)
+(132, 5)
+never talk to me again
+<class 'numpy.ndarray'>
+(20,)
+(20,)
+(132, 5)
+<class 'numpy.ndarray'>
+
+
+#train model
+pred, W, b = model(X_train, Y_train, word_to_vec_map)
+print(pred)
+
+
+#test
+print("Training set:")
+pred_train = predict(X_train, Y_train, W, b, word_to_vec_map)
+print('Test set:')
+pred_test = predict(X_test, Y_test, W, b, word_to_vec_map)
+
+#output
+Training set:
+Accuracy: 0.977272727273
+Test set:
+Accuracy: 0.857142857143
+
+
+#test
+X_my_sentences = np.array(["i adore you", "i love you", "funny lol", "lets play with a ball", "food is ready", "not feeling happy"])
+Y_my_labels = np.array([[0], [0], [2], [1], [4],[3]])
+
+pred = predict(X_my_sentences, Y_my_labels , W, b, word_to_vec_map)
+print_predictions(X_my_sentences, pred)
+
+#output
+Accuracy: 0.833333333333
+
+i adore you ❤️
+i love you ❤️
+funny lol 😄
+lets play with a ball ⚾
+food is ready 🍴
+not feeling happy 😄
+
+#test
+print(Y_test.shape)
+print('           '+ label_to_emoji(0)+ '    ' + label_to_emoji(1) + '    ' +  label_to_emoji(2)+ '    ' + label_to_emoji(3)+'   ' + label_to_emoji(4))
+print(pd.crosstab(Y_test, pred_test.reshape(56,), rownames=['Actual'], colnames=['Predicted'], margins=True))
+plot_confusion_matrix(Y_test, pred_test)
+
+#output
+(56,)
+           ❤️    ⚾    😄    😞   🍴
+Predicted  0.0  1.0  2.0  3.0  4.0  All
+Actual                                 
+0            6    0    0    1    0    7
+1            0    8    0    0    0    8
+2            2    0   16    0    0   18
+3            1    1    2   12    0   16
+4            0    0    1    0    6    7
+All          9    9   19   13    6   56
+
+
+
+
